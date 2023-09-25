41368b819283732eb2501f4068bbe0cd3749c407
hoshinki <105227818+hoshinki@users.noreply.github.com>
2023-09-25 18:06:05 +0800

    887

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const api = Router();
 api.get("/hello", (req, res) => {
 
   console.log(req.socket.address().port.toString()+" rinima");
+  
+  console.log(req.socket.localPort +" gggima");
   return res.status(200).send({ message: ima+ outerServer});
 });
 
